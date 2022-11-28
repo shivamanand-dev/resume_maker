d34077cc05881e50cc2a5ad47fd5786e38871e08
Shivam <shivam@Shivams-MacBook-Pro.local>
2022-11-29 01:19:01 +0530

    input_component

---
+++ src/components/InputField/StyledInputField.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+const StyledInputFields = styled.section`
+  width: 100%;
+  margin: 0.5rem auto;
+  .MuiInput-root,
+  .MuiInputBase-input {
+    width: 100%;
+  }
+`;
+
+export default StyledInputFields;
---
+++ src/components/InputField/index.js
@@ -0,0 +1,13 @@
+import { Input } from "@material-ui/core";
+
+import StyledInputFields from "./StyledInputField";
+
+function InputField() {
+  return (
+    <StyledInputFields>
+      <Input />
+    </StyledInputFields>
+  );
+}
+
+export default InputField;
