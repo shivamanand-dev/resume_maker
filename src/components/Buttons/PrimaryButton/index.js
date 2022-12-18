5f2996954b856b8a84b09b7ee7de7a254a7567d2
shivamanand-dev <shivam_anand@outlook.com>
2022-12-18 16:49:35 +0530

    Buttons API

--- src/components/Buttons/PrimaryButton/index.js
+++ src/components/Buttons/PrimaryButton/index.js
@@ -1,9 +1,28 @@
 import { Button } from "@material-ui/core";
 
-function PrimaryButton() {
+function PrimaryButton({
+  buttonText = "buttonText",
+  color = "primary",
+  disabled = false,
+  onClick,
+  variant = "contained",
+  size = "small",
+  fullWidth,
+  startIcon,
+  endIcon,
+}) {
   return (
-    <Button variant="contained" color="primary">
-      Primary
+    <Button
+      variant={variant}
+      color={color}
+      disabled={disabled}
+      onClick={onClick}
+      size={size}
+      fullWidth={fullWidth}
+      startIcon={startIcon}
+      endIcon={endIcon}
+    >
+      {buttonText}
     </Button>
   );
 }
