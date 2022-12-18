af1b61c7ee53b9a7d44b8bfb378ad3ac2c49d15a
shivamanand-dev <shivam_anand@outlook.com>
2022-12-19 03:45:40 +0530

    Redux integrated

--- src/components/LoginSignup/index.js
+++ src/components/LoginSignup/index.js
@@ -1,13 +1,20 @@
 import React from "react";
 import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { userService } from "src/services/user.serveice";
 
 import { PrimaryButton } from "../Buttons";
 import InputField from "../InputField";
 import StyledLoginSignup from "./StyledLoginSignup";
 
 function LoginSignup({ activeForm = "login" }) {
+  const dispatch = useDispatch();
   // eslint-disable-next-line no-unused-vars
   const [formActiveStatus, setformActiveStatus] = useState(activeForm);
+
+  function onclickLogin() {
+    userService.login(dispatch);
+  }
   return (
     <StyledLoginSignup>
       <div>
@@ -19,7 +26,7 @@ function LoginSignup({ activeForm = "login" }) {
           />
 
           <InputField type="password" placeholder="Password" name="password" />
-          <PrimaryButton />
+          <PrimaryButton buttonText="Login" onClick={onclickLogin} />
         </div>
         <div></div>
       </div>
