c9d946ae9f33ebd9dcc4094b388a9349b83c8650
Shivam <shivam@Shivams-MacBook-Pro.local>
2023-01-26 01:13:10 +0530

    logout state issue

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,25 +2,34 @@ import "../styles/globals.css";
 
 import { NoSsr } from "@mui/base";
 import { Container } from "@mui/material";
-// import { useEffect } from "react";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 import { Provider } from "react-redux";
+import { userService } from "src/services/user.service";
 import { ThemeProvider } from "styled-components";
 
 import Navbar from "@/components/Navbar";
-// import { userService } from "src/services/user.service";
 import store from "@/redux/store";
 
+import { app_routes, lockedRoutes } from "../utils/constants/app_constants";
 import { darkTheme } from "../utils/Theme/theme";
 // import { ip_data_API } from "@/utils/constants/app_config";
 
 function MyApp({ Component, pageProps }) {
-  // const user = userService?.userValue;
+  const user = userService?.userValue;
+  const router = useRouter();
 
   // useEffect(() => {
   //   if (user) {
   //     userService.updateUserCountry(ip_data_API);
   //   }
   // });
+
+  useEffect(() => {
+    if (!user && lockedRoutes.includes(router.pathname)) {
+      router.push(app_routes.login);
+    }
+  });
 
   return (
     <Provider store={store}>
