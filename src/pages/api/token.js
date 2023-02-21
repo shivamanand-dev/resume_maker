71dec428d929256a9f8c44b0bfdc888b189bec57
shivamanand-dev <shivam_anand@outlook.com>
2023-02-22 00:17:04 +0530

    bug solved

--- src/pages/api/token.js
+++ src/pages/api/token.js
@@ -1,14 +1,7 @@
+import cookie from "cookie";
+
 export default async function token(req, res) {
-  const header = req.headers.cookie.split(";");
-  const splittedHeader = header.map((e) => {
-    return e.split("=");
-  });
+  const header = cookie.parse(req.headers.cookie);
 
-  let authToken = splittedHeader.filter((word) => word[0] === " authToken");
-
-  if (authToken.length === 0) {
-    authToken = splittedHeader.filter((word) => word[0] === "authToken");
-  }
-
-  res.status(200).json({ token: authToken[0][1] });
+  res.status(200).json(header.authToken);
 }
